1aa221fccf711c48775ea5940adc99631e7d735c
Joel Earps <joel.earps@zenobe.com>
2024-09-03 08:44:06 +0100

    feat: player check functionality

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,19 @@
-import { startGame } from "./index";
+import { startGame, validateGameSettings } from "./index";
 
 describe("test application runs successfully", () => {
-  it("Should return true", () => {
+  it("Validate game settings - should return true for game to stop", () => {
     expect(startGame()).toBe(true);
+  });
+  it("Validate number of players - should return true for game to start", () => {
+    expect(validateGameSettings(5)).toBe(true);
+  });
+  it("Validate number of players - should return false for game to stop", () => {
+    expect(validateGameSettings(-1)).toBe(false);
+  });
+  it("Validate number of players - should return false for game to stop", () => {
+    expect(validateGameSettings(14)).toBe(false);
+  });
+  it("Validate number of players - should return false for game to stop", () => {
+    expect(validateGameSettings(0)).toBe(false);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
-// enum CardColour {
-//   RED = "red",
-//   GREEN = "green",
-//   BLUE = "blue",
-// }
-
-// type Card = {
-//   Value: number;
-//   Colour: CardColour;
-// };
-
-// const createCard = (): Card => {
-//   const enumPosition = Math.random(0, 4);
-//   return {
-//     Value: Math.random(0, 10),
-//     Colour: Math.random(0, 4),
-//   };
-// };
-
-// // Scenario 1: Pass in 11, 1, -4, 6: fail, fail, fail, pass
-// const dealCards = (noOfPlayer: number) => {
-//   // fail if number of players exceeds 10 or is less than 2
-//   if (noOfPlayers > 10 || noOfPlayers < 2) {
-//     return false;
-//   }
-// };
+// Use Faker to Generate random number of players
 
 export const startGame = () => {
   console.log("Hello , starting the Card Game now");
   return true;
+};
+
+export const validateGameSettings = (noOfPlayers: number) => {
+  if (noOfPlayers > 10 || noOfPlayers < 2) {
+    console.error("Invalid number of players - exiting game...");
+    return false;
+  } else {
+    console.info("Valid number of players, continuing");
+    return true;
+  }
 };
